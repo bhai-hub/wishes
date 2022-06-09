9a728a07f007c6bc2485d17c8795732fb1512096
Bhairav K sharma <51754072+bhai-hub@users.noreply.github.com>
2022-06-10 00:12:33 +0530

    time left when wishes come true

---
+++ index.js
@@ -0,0 +1,21 @@
+function countdownTimer() {
+    const difference = +new Date("2023-07-10") - +new Date();
+    let remaining = "Time's up!";
+  
+    if (difference > 0) {
+      const parts = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+      remaining = Object.keys(parts).map(part => {
+      return `${parts[part]} ${part}`;  
+      }).join(" ");
+    }
+  
+    document.getElementById("timer").innerHTML = remaining;
+  }
+  
+  countdownTimer();
+  setInterval(countdownTimer, 1000);
